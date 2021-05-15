985ebbc4b9a0da6a27eab65530a6fa1cbf634e5a
Andres Sainz <andresmsainz@gmail.com>
2021-05-14 22:10:24 -0400

    initial commit

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exception Handling!");
+
+            int x = 0;
+            int y = 3;
+            double div = 0;
+
+            try
+            {
+                div = y / x;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Error is : " + e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Result is : " + div);
+            }
+            
+
+            
+        }
+    }
+}
